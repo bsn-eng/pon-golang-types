c0ed9eeeede90c7f8a8c18889190de2b9f3cb9ae
“subhasishgoswami” <subhasishgoswami00@gmail.com>
2023-05-02 15:56:04 +0530

    PON Pool

---
+++ ponPool/types.go
@@ -0,0 +1,36 @@
+package ponpool
+
+type Builder struct {
+	BuilderPubkey string `db:"builder_pubkey" json:"id"`
+	Status        string `db:"status" json:"status"`
+}
+
+type BuilderPool struct {
+	Data struct {
+		Builders []Builder `json:"builders"`
+	} `json:"data"`
+}
+
+type Validator struct {
+	ValidatorPubkey string `db:"validator_pubkey" json:"id"`
+	Status          string `db:"status" json:"status"`
+	ReportCount     string `db:"report_count" json:"reportCount"`
+}
+
+type ValidatorPool struct {
+	Data struct {
+		Validators []Validator `json:"proposers"`
+	} `json:"data"`
+}
+
+type Reporter struct {
+	ReporterPubkey string `db:"reporter_pubkey" json:"id"`
+	Active         bool   `db:"active" json:"active"`
+	ReportCount    string `db:"report_count" json:"numberOfReports"`
+}
+
+type ReporterPool struct {
+	Data struct {
+		Reporters []Reporter `json:"reporters"`
+	} `json:"data"`
+}
