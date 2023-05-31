b7ae1d658ebd79eb701185d6b4d3372b51f9388d
“subhasishgoswami” <subhasishgoswami00@gmail.com>
2023-05-31 11:55:01 +0530

    relay

--- relay/types.go
+++ relay/types.go
@@ -1,3 +1,40 @@
 package relay
 
+import (
+	"github.com/attestantio/go-eth2-client/spec/capella"
+	"github.com/attestantio/go-eth2-client/spec/phase0"
+	"github.com/bsn-eng/pon-golang-types/RPBS"
+)
+
 type Address [20]byte
+type Signature phase0.BLSSignature
+type EcdsaAddress [20]byte
+type EcdsaSignature [65]byte
+type Hash [32]byte
+type PublicKey [48]byte
+type Transaction []byte
+
+type CapellaBuilderSubmitBlockRequest struct {
+	Signature      Signature      `json:"signature" ssz-size:"96"`
+	Message        *BidTrace      `json:"message"`
+	EcdsaSignature EcdsaSignature `json:"ecdsa_signature"`
+}
+
+type BidTrace struct {
+	Slot                 uint64    `json:"slot,string"`
+	ParentHash           Hash      `json:"parent_hash" ssz-size:"32"`
+	BlockHash            Hash      `json:"block_hash" ssz-size:"32"`
+	BuilderPubkey        PublicKey `json:"builder_pubkey" ssz-size:"48"`
+	ProposerPubkey       PublicKey `json:"proposer_pubkey" ssz-size:"48"`
+	ProposerFeeRecipient Address   `json:"proposer_fee_recipient" ssz-size:"20"`
+	GasLimit             uint64    `json:"gas_limit,string"`
+	GasUsed              uint64    `json:"gas_used,string"`
+	Value                uint64    `json:"value" ssz-size:"32"`
+
+	ExecutionPayloadHeader *capella.ExecutionPayloadHeader `json:"execution_payload_header"`
+	Endpoint               string                          `json:"endpoint"`
+	BuilderWalletAddress   Address                         `json:"builder_wallet_address"`
+	PayoutPoolTransaction  []byte                          `json:"payout_pool_transaction"`
+	RPBS                   *RPBS.EncodedRPBSSignature      `json:"rpbs"`
+	RPBSPubkey             string                          `json:"rpbs_pubkey"`
+}
--- rpbs/types.go
+++ rpbs/types.go
@@ -1,29 +1,10 @@
 package RPBS
 
-import relayTypes "github.com/bsn-eng/pon-golang-types/relay"
-
-type RpbsCommitResponse map[string]string
-type RpbsChallengeResponse map[string]string
-type RpbsSolution map[string]string
-
-type RpbsCommitMessage struct {
-	BuilderWalletAddress *relayTypes.Address `json:"builderWalletAddress"`
-	Slot                 uint64              `json:"slot"`
-	Amount               uint64              `json:"amount"`
-	TxBytes              string              `json:"txBytes"`
-}
-
-type RPBSChallenge struct {
-	Commitment RpbsCommitResponse    `json:"commitment"`
-	Challenge  RpbsChallengeResponse `json:"challenge"`
-}
-
-type ReporterSlot struct {
-	SlotLower uint64 `json:"slot_lower"`
-	SlotUpper uint64 `json:"slot_upper"`
-}
-
-type GetValidatorsResponseEntry struct {
-	Slot   uint64 `json:"slot,string"`
-	PubKey string `json:"pubKey"`
+type EncodedRPBSSignature struct {
+	Z1Hat string `json:"z1_hat"`
+	C1Hat string `json:"c1_hat"`
+	S1Hat string `json:"s1_hat"`
+	C2Hat string `json:"c2_hat"`
+	S2Hat string `json:"s2_hat"`
+	M1Hat string `json:"m1_hat"`
 }
