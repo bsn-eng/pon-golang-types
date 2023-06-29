69fe13e1f4931737cce281daf7474f0784be058a
Ronny Panford <ronnypanford@hotmail.co.uk>
2023-06-29 04:11:02 +0100

    beaconData

--- beaconclient/data.go
+++ beaconclient/data.go
@@ -31,14 +31,14 @@ type PayloadAttributesEventData struct {
 	ParentBlockNumber uint64             `json:"parent_block_number,string"`
 	ParentBlockRoot   string             `json:"parent_block_root"`
 	ParentBlockHash   string             `json:"parent_block_hash"`
-	PayloadAttributes *PayloadAttributes `json:"payload_attributes"`
+	PayloadAttributes PayloadAttributes `json:"payload_attributes"`
 }
 
 type PayloadAttributes struct {
 	Timestamp             uint64       `json:"timestamp,string"`
 	PrevRandao            string       `json:"prev_randao"`
 	SuggestedFeeRecipient string       `json:"suggested_fee_recipient"`
-	Withdrawals           *Withdrawals `json:"withdrawals"`
+	Withdrawals           Withdrawals `json:"withdrawals"`
 }
 
 type GenesisData struct {
@@ -81,7 +81,7 @@ type Withdrawal struct {
 	Amount         uint64 `json:"amount,string"`
 }
 
-type Withdrawals []*Withdrawal
+type Withdrawals []Withdrawal
 
 type BlockHeaderData struct {
 	Root      string                   `json:"root"`
